88986e3dd325de6ee201420de09a173f8c290cd6
Micha Johannes Birklbauer <19354640+michabirklbauer@users.noreply.github.com>
2026-09-23 20:10:06 +0200

    update example output

--- src/qc_bench/__init__.py
+++ src/qc_bench/__init__.py
@@ -39,24 +39,39 @@ r"""Performs quality estimation for a list of translations using LLMs from OpenA
     INFO:qc_bench._main:Using Anthropic model: False
     INFO:qc_bench._main:Using Google model: False
     INFO:qc_bench._main:Using Ollama model: True
+    INFO:qc_bench._judge:Loaded the following configuration:
+    -------------------- JudgeConfig --------------------
+    OpenAI Model:               gpt-5.4
+    OpenAI Thinking Level:      low
+    Anthropic Model:            claude-opus-5
+    Anthropic Thinking Level:   low
+    Google Model:               gemini-3.1-pro-preview
+    Google Thinking Level:      low
+    Ollama Host:                http://localhost:11434
+    Ollama Model:               gemma4:e4b
+    Ollama Keep Alive Duration: -1
+    Maximum Output Tokens:      2048
+    Maximum Retries:            5
+    Retry Waiting Time:         30.0
+    Seeds:                      1337, 10081995, 18041970, 1071966, 3082023, 24042025
+    -----------------------------------------------------
     INFO:qc_bench._main:Selected Ollama model: gemma4:e4b
-    INFO:qc_bench._util:Reading file data\test.csv...
-    INFO:qc_bench._util:Successfully read file data\test.csv!
-    Annotating data\test.csv...:   0%|                                                                                     | 0/1 [00:00<?, ?it/s]
-    INFO:httpx:HTTP Request: POST http://localhost:11434/api/generate "HTTP/1.1 200 OK"
+    INFO:qc_bench._util:Reading file data/test.csv...
+    INFO:qc_bench._util:Successfully read file data/test.csv!
+    Annotating data/test.csv...:   0%|                                                                                             | 0/1 [00:00<?, ?it/s]INFO:httpx:HTTP Request: POST http://localhost:11434/api/chat "HTTP/1.1 200 OK"
     INFO:qc_bench._judge:Successfully got a valid response after retry 0 for one query.
-    Annotating data\test.csv...: 100%|█████████████████████████████████████████████████████████████████████████████████████| 1/1 [00:08<00:00,  8.20s/it]
-    INFO:qc_bench._util:Finished annotation of data\test.csv!
+    Annotating data/test.csv...: 100%|█████████████████████████████████████████████████████████████████████████████████████| 1/1 [00:20<00:00, 20.36s/it]
+    INFO:qc_bench._util:Finished annotation of data/test.csv!
     INFO:qc_bench._util:Writing files to disk...
-    INFO:qc_bench._util:Successfully wrote file data\test_annotated.csv!
-    INFO:qc_bench._util:Successfully wrote file data\test_annotated.csv.json!
+    INFO:qc_bench._util:Successfully wrote file data/test_annotated.csv!
+    INFO:qc_bench._util:Successfully wrote file data/test_annotated.csv.json!
     shape: (1, 8)
     ┌──────────────────────────────────┬──────────────────┬──────────┬─────────┬──────────────┬─────────────────┬──────────────┬─────────────────────────┐
     │ src                              ┆ mt               ┆ src_lang ┆ mt_lang ┆ score_openai ┆ score_anthropic ┆ score_google ┆ score_ollama_gemma4:e4b │
     │ ---                              ┆ ---              ┆ ---      ┆ ---     ┆ ---          ┆ ---             ┆ ---          ┆ ---                     │
     │ str                              ┆ str              ┆ str      ┆ str     ┆ f64          ┆ f64             ┆ f64          ┆ f64                     │
     ╞══════════════════════════════════╪══════════════════╪══════════╪═════════╪══════════════╪═════════════════╪══════════════╪═════════════════════════╡
-    │ The lights are dimmable, but I…  ┆ Die Lichter sind ┆ English  ┆ German  ┆ NaN          ┆ NaN             ┆ NaN          ┆ 1.0                     │
+    │ The lights are dimmable, but I…  ┆ Die Lichter sind ┆ English  ┆ German  ┆ NaN          ┆ NaN             ┆ NaN          ┆ 0.95                    │
     │                                  ┆ dimmbar, aber…   ┆          ┆         ┆              ┆                 ┆              ┆                         │
     └──────────────────────────────────┴──────────────────┴──────────┴─────────┴──────────────┴─────────────────┴──────────────┴─────────────────────────┘
     INFO:qc_bench._main:Successfully scored and annotated all translations!
